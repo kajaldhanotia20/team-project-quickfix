12cefe3a3ab9c44fb17bb2055d8bb649d685e44b
dkinjal <dkinjal98@gmail.com>
2022-05-13 19:08:54 -0700

    No of days issue resolved

--- frontend/src/Components/Modules/bookingModal.js
+++ frontend/src/Components/Modules/bookingModal.js
@@ -66,6 +66,7 @@ export default function BookingModal({BookingDetails}) {
 
     const getDays =()=>{
         setDays(getDate(endDate)- getDate(startDate));
+        // console.log(days)
     }
     const addRooms=()=>{
         setRooms(rooms+1);
@@ -79,8 +80,8 @@ export default function BookingModal({BookingDetails}) {
     }
 
 
-    function calculatePrice(){
-        getDays();
+    async function calculatePrice(){
+        await getDays();
         console.log(days, getDay(startDate))
         // if(getDay(startDate))
         if(getDay(startDate)>5 || getDay(endDate)>5){
@@ -92,15 +93,15 @@ export default function BookingModal({BookingDetails}) {
 
     }
 
-    const handleRoomChange = (event) => {
-        setRoomType(event.target.value);
+    const handleRoomChange = async (event) => {
+        await setRoomType(event.target.value);
         let roomtype = event.target.value;
         console.log(roomtype);
         // let mapping = BookingDetails.Room_type_rate_mapping;
         console.log(mapping[roomtype]);
         let roomcost = parseFloat(mapping[roomtype]);
         // setCost(cost+ roomcost);
-        calculatePrice();
+       await  calculatePrice();
     };
 
     React.useEffect(()=>{
